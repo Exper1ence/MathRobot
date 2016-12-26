453f9cd5f86460a4cbd6dea9f61f7a1da3cbf3d1
Exper1ence <Exper1ence>
2016-12-26 16:22:04 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /**
  * Created by Exper1ence on 2016/12/26.
  */
+const fs = require('fs');
 
+process.on('uncaughtException', (err) => {
+    fs.writeSync(1, `Caught exception: ${err}`);
+});
 const router = require('./router');
 
 require('./bin/math-robot')(router);
 
-router.start(process.env.PORT||3000);
+router.start(process.env.PORT || 3000);
