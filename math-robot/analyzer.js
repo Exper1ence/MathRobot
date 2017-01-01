3aabb915f3d81f5d5b6f15f806f35c65483c5ba7
Exper1ence <3533357503@qq.com>
2017-01-01 23:43:27 +0800

    update

--- math-robot/analyzer.js
+++ math-robot/analyzer.js
@@ -5,11 +5,11 @@ const algorithm = require('./algorithms/index');
 const _ = require('lodash');
 
 module.exports = (topic, res, next) => {
-    const answers = [];
-    for (let i = 0; i < topic.questions.length; i++) {
-        const q = topic.questions[i];
-        const resolver = algorithm[topic.conditions[q].type];
-        answers.push(resolver.resolve(topic, q));
-    }
-    res.send(answers);
+    // const answers = [];
+    // for (let i = 0; i < topic.questions.length; i++) {
+    //     const q = topic.questions[i];
+    //     const resolver = algorithm[topic.conditions[q].type];
+    //     answers.push(resolver.resolve(topic, q));
+    // }
+    // res.send(answers);
 };
